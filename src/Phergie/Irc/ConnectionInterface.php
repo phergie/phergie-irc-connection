49bccbfd8bbd19441a0657f55535ad138bd29065
elazar <tobias382@gmail.com>
2012-11-04 09:50:50 -0600

    Initial commit

---
+++ src/Phergie/Irc/ConnectionInterface.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Phergie (http://phergie.org)
+ *
+ * @link http://github.com/phergie/phergie-irc-connection for the canonical source repository
+ * @copyright Copyright (c) 2008-2012 Phergie Development Team (http://phergie.org)
+ * @license http://phergie.org/license New BSD License
+ * @package Phergie\Irc
+ */
+
+namespace Phergie\Irc;
+
+/**
+ * Data structure for information related to a client connection per the IRC
+ * protocol as described in RFC 1459.
+ *
+ * @category Phergie
+ * @package Phergie\Irc
+ * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1
+ */
+interface ConnectionInterface
+{
+    /**
+     * Sets the connection password.
+     *
+     * @param string $password
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_1
+     */
+    public function setPassword($password);
+
+    /**
+     * Returns the connection password.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_1
+     */
+    public function getPassword();
+
+    /**
+     * Sets the user's nickname.
+     *
+     * @param string $nickname
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_2
+     */
+    public function setNickname($nickname);
+
+    /**
+     * Returns the user's nickname.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_2
+     */
+    public function getNickname();
+
+    /**
+     * Sets the user's username.
+     *
+     * @param string $username
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function setUsername($username);
+
+    /**
+     * Returns the user's username.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function getUsername();
+
+    /**
+     * Sets the user's hostname.
+     *
+     * @param string $hostname
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function setHostname($hostname);
+
+    /**
+     * Returns the user's hostname.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function getHostname();
+
+    /**
+     * Sets the user's server name.
+     *
+     * @param string $servername
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function setServername($servername);
+
+    /**
+     * Returns the user's server name.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function getServername();
+
+    /**
+     * Sets the user's real name.
+     *
+     * @param string $realname
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function setRealname($realname);
+
+    /**
+     * Returns the user's real name.
+     *
+     * @return string
+     * @link http://irchelp.org/irchelp/rfc/chapter4.html#c4_1_3
+     */
+    public function getRealname();
+
+    /**
+     * Sets an implementation-specific connection option.
+     *
+     * @param string $name Option name
+     * @param mixed $value Option value
+     */
+    public function setOption($name, $value);
+
+    /**
+     * Returns an implementation-specific connection option.
+     *
+     * @param string $name Option name
+     * @return mixed
+     */
+    public function getOption($name);
+}
---
+++ tests/Phergie/Irc/ConnectionTest.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Phergie (http://phergie.org)
+ *
+ * @link http://github.com/phergie/phergie-irc-connection for the canonical source repository
+ * @copyright Copyright (c) 2008-2012 Phergie Development Team (http://phergie.org)
+ * @license http://phergie.org/license New BSD License
+ * @package Phergie\Irc
+ */
+
+namespace Phergie\Irc;
+
+/**
+ * Tests for \Phergie\Irc\Connection.
+ *
+ * @category Phergie
+ * @package Phergie\Irc
+ */
+class ConnectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Instance of the class being tested
+     *
+     * @var \Phergie\Irc\Connection
+     */
+    protected $connection;
+
+    /**
+     * Instantiates the class being tested.
+     */
+    protected function setUp()
+    {
+        $this->connection = new Connection;
+    }
+
+    /**
+     * Tests setPassword().
+     */
+    public function testSetPassword()
+    {
+        $this->connection->setPassword('password');
+        $this->assertSame('password', $this->connection->getPassword());
+    }
+
+    /**
+     * Tests getPassword().
+     */
+    public function testGetPassword()
+    {
+        $this->assertNull($this->connection->getPassword());
+    }
+
+    /**
+     * Tests setNickname().
+     */
+    public function testSetNickname()
+    {
+        $this->connection->setNickname('nickname');
+        $this->assertSame('nickname', $this->connection->getNickname());
+    }
+
+    /**
+     * Tests getNickname().
+     */
+    public function testGetNickname()
+    {
+        $this->assertNull($this->connection->getNickname());
+    }
+
+    /**
+     * Tests setUsername().
+     */
+    public function testSetUsername()
+    {
+        $this->connection->setUsername('username');
+        $this->assertSame('username', $this->connection->getUsername());
+    }
+
+    /**
+     * Tests getUsername().
+     */
+    public function testGetUsername()
+    {
+        $this->assertNull($this->connection->getUsername());
+    }
+
+    /**
+     * Tests setHostname().
+     */
+    public function testSetHostname()
+    {
+        $this->connection->setHostname('hostname');
+        $this->assertSame('hostname', $this->connection->getHostname());
+    }
+
+    /**
+     * Tests getHostname().
+     */
+    public function testGetHostname()
+    {
+        $this->assertNull($this->connection->getHostname());
+    }
+
+    /**
+     * Tests setServername().
+     */
+    public function testSetServername()
+    {
+        $this->connection->setServername('servername');
+        $this->assertSame('servername', $this->connection->getServername());
+    }
+
+    /**
+     * Tests getServername().
+     */
+    public function testGetServername()
+    {
+        $this->assertNull($this->connection->getServername());
+    }
+
+    /**
+     * Tests setRealname().
+     */
+    public function testSetRealname()
+    {
+        $this->connection->setRealname('realname');
+        $this->assertSame('realname', $this->connection->getRealname());
+    }
+
+    /**
+     * Tests getRealname().
+     */
+    public function testGetRealname()
+    {
+        $this->assertNull($this->connection->getRealname());
+    }
+
+    /**
+     * Tests setOption().
+     */
+    public function testSetOption()
+    {
+        $this->connection->setOption('foo', 'bar');
+        $this->assertSame('bar', $this->connection->getOption('foo'));
+
+        $this->connection->setOption('foo', 'baz');
+        $this->assertSame('baz', $this->connection->getOption('foo'));
+
+        $this->connection->setOption('bar', 'bay');
+        $this->assertSame('bay', $this->connection->getOption('bar'));
+    }
+
+    /**
+     * Tests getOption().
+     */
+    public function testGetOption()
+    {
+        $this->assertNull($this->connection->getOption('foo'));
+        $this->assertNull($this->connection->getOption('bar'));
+    }
+}
